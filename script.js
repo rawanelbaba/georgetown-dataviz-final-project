c5427caca2174813c8bda76a772b3eeba9e1141a
rawanelbaba <42986065+rawanelbaba@users.noreply.github.com>
2018-12-05 14:39:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ d3.csv('Journalists-Killed.csv').then((data) => {
             .attr('height', (d) => {
               return yScale.range()[1]-yScale(d[keyToUse])
             })
-            .attr('fill','fill-opacity', 0.5)
+            .attr('fill','red')
             .attr('class', 'mark')
         
         bars.on('mouseover', (d) => {
